c21f6ff00098e0f06e821bfd84dd9f9c5870982d
xindongdong <dev.simdd@gmail.com>
2019-04-23 13:51:18 +0800

    add: two sum

---
+++ two-sum/two_sum.go
@@ -0,0 +1,13 @@
+package two_sum
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		rest := target - nums[i]
+		if _, ok := m[rest]; ok {
+			return []int{m[rest], i}
+		}
+		m[nums[i]] = i
+	}
+	return nil
+}
---
+++ two-sum/two_sum_test.go
@@ -0,0 +1,26 @@
+package two_sum
+
+import "testing"
+
+func TestTwoSum(t *testing.T) {
+	origin := [][]int{
+		[]int{1, 2, 3, 4},
+		[]int{2, 3, 5},
+	}
+
+	target := []int{
+		6,
+		7,
+	}
+
+	result := [][]int{
+		[]int{1, 3},
+		[]int{0, 2},
+	}
+
+	for i := 0; i < len(origin); i++ {
+		if ret := twoSum(origin[i], target[i]); ret[0] != result[i][0] && ret[1] != result[i][1] {
+			t.Fatalf("case %d fails: %v\n", i, ret)
+		}
+	}
+}
