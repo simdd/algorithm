88651caec2049b59cd5ac7e4aa2e605293baf778
SIMDD <dev.simdd@gmail.com>
2019-04-28 22:24:28 +0800

    feat: 🎸 动态规划：素数

---
+++ prime-number/prime_number.go
@@ -0,0 +1,26 @@
+package prime_number
+
+func primeNumber(o int) []int {
+	var arr []int
+
+	if o > 1 {
+		arr = append(arr, 2)
+	}
+
+	for i:=3; i<o; i++ {
+		res := false
+
+		for _, v := range arr {
+			if i % v == 0 {
+				res = true
+				break
+			}
+		}
+
+		if res == false {
+			arr= append(arr, i)
+		}
+	}
+
+	return arr
+}
---
+++ prime-number/prime_number_test.go
@@ -0,0 +1,36 @@
+package prime_number
+
+import "testing"
+import "fmt"
+
+func StringSliceEqualBCE(a, b []int) bool {
+    if len(a) != len(b) {
+        return false
+    }
+
+    if (a == nil) != (b == nil) {
+        return false
+    }
+
+    b = b[:len(a)]
+    for i, v := range a {
+        if v != b[i] {
+            return false
+        }
+    }
+
+    return true
+}
+
+func TestPrimeNumber(t *testing.T)  {
+	origin := 40
+
+	target := []int {
+		2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+	}
+
+    res := StringSliceEqualBCE(primeNumber(origin), target)
+
+    fmt.Printf("%v\n", res)
+    fmt.Printf("%v", primeNumber(origin))
+}
