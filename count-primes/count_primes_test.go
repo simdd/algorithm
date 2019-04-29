5c16c5b06b7fb2052eb19e966290083eb71441cf
SIMDD <dev.simdd@gmail.com>
2019-04-29 21:18:13 +0800

    feat: 🎸 leetcode count primes

---
+++ count-primes/count_primes_test.go
@@ -0,0 +1,14 @@
+package count_primes
+
+import "testing"
+import "fmt"
+
+func TestCountPrimes(t *testing.T) {
+	origin := 40
+
+	target := []int{
+		2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
+	}
+
+	fmt.Printf("%v", countPrimes(origin) == len(target))
+}
