ecb4f0efa6b9bc3d831b13eb584cc3bae68ee9d9
SIMDD <dev.simdd@gmail.com>
2019-07-15 08:59:21 +0800

    update: count and say

--- count-and-say/main.go
+++ count-and-say/main.go
@@ -1,9 +1,32 @@
 package problem
 
-import "testing"
+// 1.     1
+// 2.     11
+// 3.     21
+// 4.     1211
+// 5.     111221
 
-func TestCountAndSay(t *testing.T) {
-	for i := 0; i <= 30; i++ {
-		println(countAndSay(i))
+// read([2,1]) [1,2,1,1]
+func read(n []int) []int {
+	val := n[0]
+	count := 1
+	ret := []int{}
+
+	for i:=1; i<len(n); i++ {
+		if n[i] == val {
+			count++
+		}else {
+			ret = append(ret, count)
+			ret = append(ret, val)
+			val = n[i]
+			count = 1
+		}
 	}
+
+	ret = append(ret, count)
+	ret = append(ret, val)
+
+	return ret
 }
+
+// func countAndSay(n int) string {}
