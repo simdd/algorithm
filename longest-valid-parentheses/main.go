36927907fc08e5e086e3216a5bec71c73eb5a7cb
SIMDD <dev.simdd@gmail.com>
2019-07-16 14:29:32 +0800

    feat: 🎸 add

--- longest-valid-parentheses/main.go
+++ longest-valid-parentheses/main.go
@@ -1,2 +1,31 @@
 package problem
 
+import (
+	"strings"
+)
+
+func longestValidParenthesesByCount(s string) []string {
+	origin := strings.Split(s, "")
+	maxlen := 0
+	left := 0
+	right := 0
+
+	for _, v := range origin {
+		if v == "(" {
+			left++
+		} else if v == ")" {
+			right++
+		}
+
+		if right > left {
+			if maxlen < right+left {
+				maxlen = right + left
+			}
+
+			right = 0
+			left = 0
+		}
+	}
+
+	return origin
+}
--- longest-valid-parentheses/main_test.go
+++ longest-valid-parentheses/main_test.go
@@ -1,2 +1,11 @@
-
 package problem
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestLongestValidParenthesesByCount(t *testing.T) {
+	ret := longestValidParenthesesByCount("(())))(()")
+	fmt.Println(ret)
+}
