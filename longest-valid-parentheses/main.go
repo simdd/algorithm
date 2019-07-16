1396a0677d6e16e543459bb5fea15965117eeb34
simdd <dev.simdd@gmail.com>
2019-07-16 21:28:44 +0800

    review

--- longest-valid-parentheses/main.go
+++ longest-valid-parentheses/main.go
@@ -7,23 +7,25 @@ import (
 func longestValidParenthesesByCount(s string) int {
 	origin := strings.Split(s, "")
 
-	maxlenleft := 0
-	maxlenright := 0
+	var maxlenleft int
+	var maxlenright int
 
-	left := 0
-	right := 0
+	var left int
+	var right int
 
 	for _, v := range origin {
 		if v == "(" {
 			left++
-		} else if v == ")" {
+		} else {
 			right++
 		}
 
 		if right > left {
 			right = 0
 			left = 0
-		} else if right == left {
+		}
+
+		if right == left {
 			if maxlenleft < left+right {
 				maxlenleft = right + left
 			}
@@ -38,14 +40,16 @@ func longestValidParenthesesByCount(s string) int {
 
 		if v == "(" {
 			left++
-		} else if v == ")" {
+		} else {
 			right++
 		}
 
 		if left > right {
 			right = 0
 			left = 0
-		} else if left == right {
+		}
+
+		if left == right {
 			if maxlenright < right+left {
 				maxlenright = left + right
 			}
