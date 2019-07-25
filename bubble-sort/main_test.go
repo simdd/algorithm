0f1c0add576d2f2ba06e8234104705ff94196217
SIMDD <dev.simdd@gmail.com>
2019-07-25 13:37:43 +0800

    feat: utils fmt

--- bubble-sort/main_test.go
+++ bubble-sort/main_test.go
@@ -1,12 +1,12 @@
 package problem
 
 import (
+	"algorithm/utils"
 	"fmt"
 	"testing"
-	"algorithm/utils"
 )
 
 func TestMain(t *testing.T) {
-	ret := utils.IntSliceEqualBCE(main2([]int{1,3,2,4}), []int{1,2,3})
+	ret := utils.IntSliceEqualBCE(main2([]int{1, 3, 2, 4}), []int{1, 2, 3})
 	fmt.Println(ret)
 }
--- prime-number/main_test.go
+++ prime-number/main_test.go
@@ -1,26 +1,10 @@
 package problem
 
-import "testing"
-import "fmt"
-
-func StringSliceEqualBCE(a, b []int) bool {
-	if len(a) != len(b) {
-		return false
-	}
-
-	if (a == nil) != (b == nil) {
-		return false
-	}
-
-	b = b[:len(a)]
-	for i, v := range a {
-		if v != b[i] {
-			return false
-		}
-	}
-
-	return true
-}
+import (
+	"algorithm/utils"
+	"fmt"
+	"testing"
+)
 
 func TestPrimeNumber(t *testing.T) {
 	origin := 40
@@ -29,7 +13,7 @@ func TestPrimeNumber(t *testing.T) {
 		2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37,
 	}
 
-	res := StringSliceEqualBCE(primeNumber(origin), target)
+	res := utils.IntSliceEqualBCE(primeNumber(origin), target)
 
 	fmt.Printf("%v\n", res)
 	fmt.Printf("%v", primeNumber(origin))
