7454e9f09e31a435246df0939e41361d8c2ef6a3
simdd <dev.simdd@gmail.com>
2019-08-14 22:49:33 +0800

    fix: overflow

--- reverse-integer/main.go
+++ reverse-integer/main.go
@@ -1,12 +1,14 @@
 package problem
 
-func main(x int) (out int) {
-	if x-int(int32(x)) > 0 {
-		return 0
-	}
+import "math"
 
+func main(x int) (out int) {
 	for ; x != 0; x /= 10 {
 		out = out*10 + x%10
+
+		if out > math.MaxInt32 || out < -math.MaxInt32-1 {
+			return 0
+		}
 	}
 	return out
 }
